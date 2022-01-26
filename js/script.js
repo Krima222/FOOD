4ce8d83fd70de785675dc0dcb11acb50206a9ea2
Krima222 <manackaakristina@gmail.com>
2022-01-27 00:33:10 +0300

    54 add user alerts

--- js/script.js
+++ js/script.js
@@ -104,8 +104,8 @@ window.addEventListener("DOMContentLoaded", () => {
 //         
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close]');
+          modal = document.querySelector('.modal');
+
 
     function openModal () {
         modal.classList.add('show');
@@ -127,11 +127,11 @@ window.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = '';
     }
     
-    modalCloseBtn.addEventListener('click', closeModal);
+
 
     //при клике на область, где нет модального окна, оно закрывается 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
             closeModal();
         }
     });
@@ -143,7 +143,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 3000);
+    const modalTimerId = setTimeout(openModal, 50000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -233,7 +233,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const forms = document.querySelectorAll('form');
     //текстовое сообщение, если что-то произошло
     const message = {
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что-то пошло не так...'
     };
@@ -248,10 +248,13 @@ window.addEventListener("DOMContentLoaded", () => {
             //чтобы отменить стандартное поведение браузера
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent=message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText =`
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
@@ -270,16 +273,39 @@ window.addEventListener("DOMContentLoaded", () => {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
-                    statusMessage.textContent=message.success;
+                    showThanksModal(message.success);
+                    statusMessage.remove();
                     form.reset();
-                    setTimeout( () => {
-                        statusMessage.remove();
-                    }, 2000);
                 } else {
-                    statusMessage.textContent=message.failure;
+                    showThanksModal(message.failure);
                 }
             });
         });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+        <div class="modal__content">
+            <div class="modal__close" data-close>&times;</div>
+            <div class="modal__title">${message}</div>
+        </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            closeModal();
+        }, 4000);
+
     }
 });
 
