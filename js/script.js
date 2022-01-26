a7eb7a97e857bd65f4eec1b1465ac02fbe104535
Krima222 <manackaakristina@gmail.com>
2022-01-26 21:36:52 +0300

    53 add sending to the server

--- js/script.js
+++ js/script.js
@@ -226,7 +226,61 @@ window.addEventListener("DOMContentLoaded", () => {
         '21',
             '.menu__field .container'
         ).addText();
-    
+
+
+
+    //Forms
+    const forms = document.querySelectorAll('form');
+    //текстовое сообщение, если что-то произошло
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        //срабатывает каждый раз, когда хотим отправить какую-то форму
+        form.addEventListener('submit', (e) => {
+            //чтобы отменить стандартное поведение браузера
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent=message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json =JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent=message.success;
+                    form.reset();
+                    setTimeout( () => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent=message.failure;
+                }
+            });
+        });
+    }
 });
 
 
